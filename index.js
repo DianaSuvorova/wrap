1fd8e2a0d152be6b00d2a3fbdc93d14d959dce38
Diana Suvorova <diana.suvorova@uber.com>
2021-10-25 09:36:36 -0700

    repro

---
+++ index.js
@@ -0,0 +1,96 @@
+import { fetch } from 'cross-fetch';
+import { print } from 'graphql';
+import { wrapSchema } from '@graphql-tools/wrap';
+import gql from 'graphql-tag';
+
+const typeDefs = gql`
+#
+type Query {
+  # Get a user by id
+  user(
+    # The id of the user
+    id: String
+  ): User
+
+  # Get all users
+  users: [User]
+
+  # Get all todos
+  todos: [Todo]
+}
+
+#
+input UpdateTodoInput {
+  #
+  id: String
+
+  #
+  description: String
+
+  #
+  done: Boolean
+}
+
+#
+type User {
+  #
+  id: String
+
+  #
+  name: String
+
+  #
+  email: String
+
+  #
+  todos: [Todo]
+}
+
+#
+type Todo {
+  #
+  id: String
+
+  #
+  description: String
+
+  #
+  done: Boolean
+}
+
+#
+type Mutation {
+  #
+  updateTodo(
+    # Todo input
+    input: UpdateTodoInput!
+  ): Todo
+}
+`;
+
+const executor = async ({ document, variables }) => {
+  const query = print(document);
+  const fetchResult = await fetch('https://api.mocki.io/v2/c4d7a195/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query, variables }),
+  });
+  return fetchResult.json();
+};
+
+
+
+const schema =  wrapSchema({
+    schema: typeDefs,
+    executor,
+});
+
+console.log(schema);
+
+
+
+
+
+
